86ff22ec76a2dec0753f5ed5c931a63b94767d9e
ShutterAperture <ShutterAperture@users.noreply.github.com>
2021-09-22 10:24:19 -0400

    Add unit tests for solution entry component

--- src/app/concentration/components/solution-entry/solution-entry.component.spec.ts
+++ src/app/concentration/components/solution-entry/solution-entry.component.spec.ts
@@ -24,4 +24,29 @@ describe('SolutionEntryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('checkSolution', () => {
+    it('should emit out the entered solution via solutionProposed', () => {
+      spyOn(component.solutionProposed, 'emit')
+      component.solutionForm.get('solution')?.patchValue('This is the solution');
+      component.checkSolution();
+      expect(component.solutionProposed.emit).toHaveBeenCalledWith('This is the solution')
+    })
+  });
+
+  describe('cancelSolve', () => {
+    it('should emit out null via solutionProposed', () => {
+      spyOn(component.solutionProposed, 'emit')
+      component.cancelSolve();
+      expect(component.solutionProposed.emit).toHaveBeenCalledWith(null)
+    })
+  });
+
+  describe('giveUp', () => {
+    it('should emit out ____________', () => {
+      spyOn(component.giveUpGame, 'emit')
+      component.giveUp();
+      expect(component.giveUpGame.emit).toHaveBeenCalled()
+    })
+  });
 });
