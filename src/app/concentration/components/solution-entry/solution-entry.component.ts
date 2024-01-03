be849571ac3f0ca1ae9096b7adb57a9c984b8fa4
ShutterAperture <ShutterAperture@users.noreply.github.com>
2024-01-02 22:49:09 -0500

    Adjust font size and padding large prizes, add more prizes, add autofocus of solution form on match.

--- src/app/concentration/components/solution-entry/solution-entry.component.ts
+++ src/app/concentration/components/solution-entry/solution-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, AfterViewInit,  Output, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,17 +6,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './solution-entry.component.html',
   styleUrls: ['./solution-entry.component.scss']
 })
-export class SolutionEntryComponent implements OnInit {
+export class SolutionEntryComponent implements OnInit, AfterViewInit  {
   @Input() showGiveUp = false;
   @Output() solutionProposed: EventEmitter<string|null> = new EventEmitter<string | null>()
   @Output() giveUpGame: EventEmitter<void> = new EventEmitter<void>()
   solutionForm!: FormGroup;
+
+  @ViewChild("solutionInput") solutionInput!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
     this.solutionForm = new FormGroup({
       solution: new FormControl('', [ Validators.required,])
     });
+  }
+
+  ngAfterViewInit() {
+    this.solutionInput.nativeElement.focus();
   }
 
   checkSolution(evt: Event) {
